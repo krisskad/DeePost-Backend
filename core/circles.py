8a68119d81e575cc0cc3a2e4ec431116c8d47891
krishna_kadam <krishna.kadam@ongil.io>
2021-10-14 13:32:01 +0530

    core functions for image processing

---
+++ core/circles.py
@@ -0,0 +1,7 @@
+def circle1(width=None, height=None, antilising=None, blur=None, sharpness=None):
+    if width and height:
+        size = (width, height)
+
+    # Write code here
+
+    return img
